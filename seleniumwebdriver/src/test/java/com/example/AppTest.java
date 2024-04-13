d19bc71e40175d0ed43cb83688ead5e97eb39d06
sushmithanagamuthu <124781948+sushmithanagamuthu@users.noreply.github.com>
2024-04-13 11:51:18 +0530

    Add files via upload

---
+++ seleniumwebdriver/src/test/java/com/example/AppTest.java
@@ -0,0 +1,25 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.shoppersstop.com/");
+        assertTrue(true);
+    }
+}
